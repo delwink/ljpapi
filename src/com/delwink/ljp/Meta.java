324701c909be7d5c49e8f0b18eaba9dd6c5b189c
David McMackins II <contact@mcmackins.org>
2015-08-13 06:54:14 -0500

    Documented version information

--- src/com/delwink/ljp/Meta.java
+++ src/com/delwink/ljp/Meta.java
@@ -22,6 +22,26 @@ package com.delwink.ljp;
  * @author David McMackins II
  */
 public class Meta {
-    public static final int MAJOR_VERSION = 0, MINOR_VERSION = 0, PATCH = 0;
+    /**
+     * Primary version of the library, incremented after an incompatible
+     * interface change.
+     */
+    public static final int MAJOR_VERSION = 0;
+    
+    /**
+     * Secondary version of the library, incremented after an interface change
+     * which is compatible with the previous version.
+     */
+    public static final int MINOR_VERSION = 0;
+    
+    /**
+     * Tertiary version of the library, incremented after a change which does
+     * not involve the API itself, such as a bug fix.
+     */
+    public static final int PATCH = 0;
+    
+    /**
+     * A complete version string based on the version numbers.
+     */
     public static final String VERSION = String.format("%d.%d.%d", MAJOR_VERSION, MINOR_VERSION, PATCH);
 }
